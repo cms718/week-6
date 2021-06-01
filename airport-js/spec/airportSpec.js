cdc08852ad63d81c719258ffa0df1e22377ba852
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-06-01 17:31:47 +0100

    airport land function fails when airport is full

--- airport-js/spec/airportSpec.js
+++ airport-js/spec/airportSpec.js
@@ -1,18 +1,23 @@
 describe("Airport", function() {
-  let plane = new Plane();
-  let airport = new Airport();
+  let plane;
+  let airport;
+
+  beforeEach(() => {
+    plane = new Plane();
+    airport = new Airport();
+  })
 
   describe("land", function() {
     it("orders a plane to land", function () {
       airport.land(plane);
       expect(plane.getState()).toEqual("landed")
     });
-    // it("fails when the airport is full", function (){
-    //   for(i = 1; i <= 20; i++) {
-    //     airport.land(new Plane());
-    //   }
-    //   expect
-    // })
+    it("fails when the airport is full", function (){
+      for(i = 1; i <= 20; i++) {
+        airport.land(new Plane());
+      }
+      expect( function(){ airport.land(plane); } ).toThrowError('The airport is full');
+    })
   });
 
   describe("takeoff", function(){
--- airport-js/src/airport.js
+++ airport-js/src/airport.js
@@ -1,9 +1,18 @@
 class Airport {
+  constructor() {
+    this.planes = []
+  }
+
   land(plane) {
-    plane.land()
+    if(this.planes.length == 20) {
+      throw new Error('The airport is full');
+    } else {
+      this.planes.push(plane);
+      plane.land();
+    }
   }
 
   takeoff(plane) {
-    plane.takeoff()
+    plane.takeoff();
   }
 }
