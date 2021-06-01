70999dc77318750194130e566c09cc751ce4507f
Charlie Slater <charliemslater1701@gmail.com>
2021-06-01 15:58:08 +0100

    returns fizz and fizzbuzz

--- fizz-buzz-js/spec/fizzBuzzSpec.js
+++ fizz-buzz-js/spec/fizzBuzzSpec.js
@@ -2,11 +2,26 @@ describe("Fizz Buzz", function () {
   let fizzBuzz = new FizzBuzz();
 
   describe("multiples of 3", function () {
-    it("returns fizz for 3", function () {
+    it("returns Fizz for 3", function () {
       expect(fizzBuzz.play(3)).toEqual("Fizz");
     });
-    it("returns fizz for 6", function () {
+    it("returns Fizz for 6", function () {
       expect(fizzBuzz.play(6)).toEqual("Fizz");
+    });
+  });
+
+  describe("multiples of 5", function () {
+    it("returns Buzz for 5", function () {
+      expect(fizzBuzz.play(5)).toEqual("Buzz");
+    });
+    it("returns Buzz for 10", function () {
+      expect(fizzBuzz.play(10)).toEqual("Buzz");
+    });
+  });
+
+  describe("multiples of 5 and 3", function () {
+    it("returns FizzBuzz for 15", function () {
+      expect(fizzBuzz.play(15)).toEqual("FizzBuzz");
     });
   });
 });
--- fizz-buzz-js/src/fizzBuzz.js
+++ fizz-buzz-js/src/fizzBuzz.js
@@ -1,5 +1,11 @@
 class FizzBuzz {
   play = (num) => {
-    return "Fizz";
+    if (num % 3 == 0 && num % 5 == 0) {
+      return "FizzBuzz";
+    } else if (num % 3 == 0) {
+      return "Fizz";
+    } else if (num % 5 == 0) {
+      return "Buzz";
+    }
   };
 }
