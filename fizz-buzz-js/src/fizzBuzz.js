1eac6491bf884c5f212c9675d4776bdcf20e1b17
Charlie Slater <charliemslater1701@gmail.com>
2021-06-01 16:02:15 +0100

    refactor

--- fizz-buzz-js/src/fizzBuzz.js
+++ fizz-buzz-js/src/fizzBuzz.js
@@ -1,13 +1,12 @@
 class FizzBuzz {
   play = (num) => {
-    if (num % 3 == 0 && num % 5 == 0) {
-      return "FizzBuzz";
-    } else if (num % 3 == 0) {
-      return "Fizz";
-    } else if (num % 5 == 0) {
-      return "Buzz";
-    } else {
-      return num;
+    let result = "";
+    if (num % 3 == 0) {
+      result += "Fizz";
     }
+    if (num % 5 == 0) {
+      result += "Buzz";
+    }
+    return result ? result : num;
   };
 }
